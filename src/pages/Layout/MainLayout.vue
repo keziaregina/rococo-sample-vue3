4c468058cf3603f27b53b5286fb14ed2f01640e6
Kezia Regina <keziaregina28@gmail.com>
2025-11-21 18:37:15 +0700

    feat(layout): add MainLayout component for the Simple Todo App

---
+++ src/pages/Layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <!-- <q-btn flat round icon="menu" @click="toggleLeftDrawer" /> -->
+        <q-toolbar-title>Simple Todo App</q-toolbar-title>
+        <q-btn flat round icon="account_circle" @click="toggleUserMenu" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="q-pa-xl q-mt-xl"> <router-view /> </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { useAuthStore } from 'stores/auth'
+import { useRouter } from 'vue-router'
+import { onMounted, computed, ref } from 'vue'
+</script>
