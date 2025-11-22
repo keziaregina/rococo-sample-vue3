3094a657c1b05d7c65f0ac20d5cfcba89a151727
Kezia Regina <keziaregina28@gmail.com>
2025-11-22 18:28:07 +0700

    feat(layout): implement logout confirmation dialog in MainLayout component

--- src/pages/Layout/MainLayout.vue
+++ src/pages/Layout/MainLayout.vue
@@ -12,7 +12,7 @@
               <q-item-section> Edit User </q-item-section>
             </q-item>
 
-            <q-item clickable @click="authStore.logout()">
+            <q-item clickable @click="openLogoutDialog = true">
               <q-item-section> Logout </q-item-section>
             </q-item>
           </q-list>
@@ -25,7 +25,18 @@
       @close="openEditUserModal = false"
       :onSubmit="handleSubmitEditUser"
     />
-
+    <q-dialog v-model="openLogoutDialog">
+      <q-card style="width: 50vw">
+        <q-card-section class="q-pa-md">
+          <div class="text-h6">Logout</div>
+          <div class="text-subtitle2">Are you sure you want to logout?</div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn label="Cancel" color="secondary" @click="openLogoutDialog = false" />
+          <q-btn label="Logout" color="primary" @click="handleLogout()" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <q-page-container class="q-pa-xl q-mt-xl"> <router-view /> </q-page-container>
   </q-layout>
 </template>
@@ -38,6 +49,7 @@ import EditUserModal from 'src/components/EditUserModal.vue'
 const authStore = useAuthStore()
 const userMenu = ref(false)
 const openEditUserModal = ref(false)
+const openLogoutDialog = ref(false)
 
 const toggleUserMenu = () => {
   userMenu.value = !!userMenu.value
@@ -50,5 +62,10 @@ const goToEditUser = () => {
 const handleSubmitEditUser = async (payload) => {
   await authStore.updateUser(payload)
   openEditUserModal.value = false
+}
+
+const handleLogout = () => {
+  authStore.logout()
+  openLogoutDialog.value = false
 }
 </script>
